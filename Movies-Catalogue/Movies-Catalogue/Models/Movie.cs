5fb56284fe1bed622de2954a96a968259e58d2cc
JuMaxi <jmaximovski@gmail.com>
2023-03-20 19:33:37 +0000

    Class Movie

--- Movies-Catalogue/Movies-Catalogue/Models/Movie.cs
+++ Movies-Catalogue/Movies-Catalogue/Models/Movie.cs
@@ -10,12 +10,12 @@ namespace Movies_Catalogue.Models
         public string CoverImage { get; set; }
         public DateTime ReleaseDate { get; set; }
         public double Rating { get; set; }
-        public DateTime Length { get; set; }
+        public int Length { get; set; }
         public BoxOffice BoxOffice { get; set; } // Id boxoffice
         public string Origin { get; set; }
         public string Locations { get; set; }    
         public List<ActorRole> ActorRole { get; set; } // id actor
-        public List<MovieGender> GenderId { get; set; } // Id moviegender
+        public MovieGender GenderId { get; set; } // Id moviegender
         public Producer ProducerId { get; set; } // Id producer
     }
 }
--- Movies-Catalogue/Movies-Catalogue/Services/AddMovie.cs
+++ Movies-Catalogue/Movies-Catalogue/Services/AddMovie.cs
@@ -13,15 +13,7 @@ namespace Movies_Catalogue.Services
         public void NewMovie (Movie New)
         {
             string Insert1 = "insert into Movies (Title, CoverImage, ReleaseDate, Rating, LengthM, Origin, Locations, GenderId, ProducerId) values ('";
-            Insert1 = Insert1 + New.Title + "','" + New.CoverImage + "','" + New.ReleaseDate.ToString("yyyy-MM-dd") + "'," + New.Rating + ",'" + New.Length.ToString("m") + "','" + New.Origin + "','" + New.Locations;
-
-            for(int PositionGender = 0; PositionGender < New.GenderId.Count; PositionGender++)
-            {
-                string Temp = "," + Convert.ToString(New.GenderId[PositionGender].Id);
-                Insert1 = Insert1 + Temp;
-            }
-
-            Insert1 = Insert1 + New.ProducerId.Id + ",)";
+            Insert1 = Insert1 + New.Title + "','" + New.CoverImage + "','" + New.ReleaseDate.ToString("yyyy-MM-dd") + "'," + New.Rating + "," + New.Length + ",'" + New.Origin + "','" + New.Locations + "','" + New.GenderId.Id + "," + New.ProducerId.Id + ")"; ;
 
             AccessDB.AccessNonQuery(Insert1);
         }
