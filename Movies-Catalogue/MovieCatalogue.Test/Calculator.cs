c97240d362428f1971c893e0c2f8e87b721226b0
JuMaxi <jmaximovski@gmail.com>
2023-04-11 14:24:12 +0100

    White Box Tests - Unit Tests

---
+++ Movies-Catalogue/MovieCatalogue.Test/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalogue.Test
+{
+    public class Calculator
+    {
+        public int Somar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public int Subtrair(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        public int Mulitiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public int Divisao(int numero1, int numero2)
+        {
+            if (numero2 == 0)
+            {
+                throw new Exception("It's impossible to divid one number for zero. Change the number to continue.");
+            }
+            return numero1 / numero2;
+        }
+
+        public int Potencia(int numero1, int numero2)
+        {
+            int resultado = 1; 
+            for(int Position = 0; Position < numero2; Position++)
+            {
+                resultado = resultado * numero2;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Movies-Catalogue/MovieCatalogue.Test/MovieResponseTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Movies_Catalogue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MovieCatalogue.Test
+{
+    public class MovieResponseTests
+    {
+        [Fact]
+
+        public void GivenThatIHaveAMovieResponse_WhenIAddALocation_ThenItShouldAppearLocationToListLocations()
+        {
+            string Location = "Brazil";
+
+            MovieResponse MovieLocation = new MovieResponse();
+
+            MovieLocation.AddFilmingLocation(Location);
+            MovieLocation.Locations.Should().HaveCount(1);
+        }
+    }
+}
